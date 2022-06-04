8dd4fdf222bfad36f4f5ecf7b82b45fef5d3b7a4
a-fujimt <a-fujimt@ist.osaka-u.ac.jp>
2022-06-04 23:50:27 +0900

    [fix] schema

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -3,7 +3,7 @@ set schema s_share;
 
 create table if not exists account
 (
-    id                 bigint unsigned       primary key  auto_increment,
+    account_id         bigint unsigned       primary key  auto_increment,
     user_id            varchar(15)           unique       not null,
     user_name          varchar(30)           unique       not null,
     password           char(60)                           not null,
@@ -15,18 +15,18 @@ create table if not exists account
 
 create table if not exists friend
 (
-    id           bigint unsigned      primary key   auto_increment,
-    user_id_1    bigint unsigned                    not null,
-    user_id_2    bigint unsigned                    not null,
-    status       smallint unsigned    default 0     not null,
-    updated_at   timestamp(3)         default current_timestamp(3) not null,
+    friend_id       bigint unsigned      primary key   auto_increment,
+    account_id_1    bigint unsigned                    not null,
+    account_id_2    bigint unsigned                    not null,
+    status          smallint unsigned    default 0     not null,
+    updated_at      timestamp(3)         default current_timestamp(3) not null,
     foreign key (user_id_1) references account (id),
     foreign key (user_id_2) references account (id)
 );
 
 create table if not exists shussha
 (
-    id           bigint unsigned       primary key          auto_increment,
+    shussha_id   bigint unsigned       primary key          auto_increment,
     user_id      bigint unsigned                            not null,
     date         date            default current_date,
     updated_at   timestamp(3)    default current_timestamp(3) not null,
