b8d216b38d7983fdb2601110ba04a65dd75c909c
a-fujimt <a-fujimt@ist.osaka-u.ac.jp>
2022-06-05 19:19:29 +0900

    [delete] 不要なAccount検索

--- src/main/java/net/akichil/shusshare/repository/AccountRepository.java
+++ src/main/java/net/akichil/shusshare/repository/AccountRepository.java
@@ -1,17 +1,8 @@
 package net.akichil.shusshare.repository;
 
 import net.akichil.shusshare.entity.Account;
-import net.akichil.shusshare.entity.UserSelector;
-
-import java.util.List;
 
 public interface AccountRepository {
-
-    List<Account> findAll(UserSelector selector);
-
-    List<Account> findFriend(Integer id);
-
-    List<Account> findGoOfficeFriend(Integer id);
 
     Account findOne(Integer id);
 
--- src/main/java/net/akichil/shusshare/repository/AccountRepositoryImpl.java
+++ src/main/java/net/akichil/shusshare/repository/AccountRepositoryImpl.java
@@ -1,12 +1,8 @@
 package net.akichil.shusshare.repository;
 
 import net.akichil.shusshare.entity.Account;
-import net.akichil.shusshare.entity.UserSelector;
-import net.akichil.shusshare.repository.mybatis.AccountMapper;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public class AccountRepositoryImpl implements AccountRepository {
@@ -15,21 +11,6 @@ public class AccountRepositoryImpl implements AccountRepository {
 
     public AccountRepositoryImpl(SqlSession sqlSession) {
         this.sqlSession = sqlSession;
-    }
-
-    @Override
-    public List<Account> findAll(UserSelector selector) {
-        return sqlSession.getMapper(AccountMapper.class).findAll(selector);
-    }
-
-    @Override
-    public List<Account> findFriend(Integer id) {
-        return null;
-    }
-
-    @Override
-    public List<Account> findGoOfficeFriend(Integer id) {
-        return null;
     }
 
     @Override
@@ -51,4 +32,5 @@ public class AccountRepositoryImpl implements AccountRepository {
     public void remove(Account account) {
 
     }
+
 }
