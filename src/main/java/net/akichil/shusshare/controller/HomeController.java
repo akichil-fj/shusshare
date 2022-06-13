4841378c4c84f68e52e462524efa2090301704ed
akichil-fj <a-fujimt@ist.osaka-u.ac.jp>
2022-06-13 23:45:39 +0900

    [add] 出社ボタン実装

--- src/main/java/net/akichil/shusshare/controller/HomeController.java
+++ src/main/java/net/akichil/shusshare/controller/HomeController.java
@@ -1,5 +1,6 @@
 package net.akichil.shusshare.controller;
 
+import net.akichil.shusshare.entity.Shussha;
 import net.akichil.shusshare.repository.exception.ResourceNotFoundException;
 import net.akichil.shusshare.security.LoginUser;
 import net.akichil.shusshare.service.AccountService;
@@ -9,6 +10,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDate;
@@ -39,6 +41,21 @@ public class HomeController {
             model.addAttribute("shussha", null);
         }
         return "home";
+    }
+
+    @PostMapping(value = "/shussha/create")
+    public String addShussha(Model model, @AuthenticationPrincipal LoginUser loginUser) {
+        Shussha shussha = new Shussha();
+        shussha.setDate(LocalDate.now());
+        shussha.setAccountId(loginUser.getAccountId());
+        shusshaService.add(shussha);
+        return "redirect:/home";
+    }
+
+    @PostMapping(value = "/shussha/remove")
+    public String removeShussha(Model model, @AuthenticationPrincipal LoginUser loginUser) {
+        shusshaService.remove(loginUser.getAccountId(), LocalDate.now());
+        return "redirect:/home";
     }
 
 }
