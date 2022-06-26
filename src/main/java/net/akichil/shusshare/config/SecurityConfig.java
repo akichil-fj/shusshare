cf7de425dcea322301530b725e1938903401db1c
akichil-fj <a-fujimt@ist.osaka-u.ac.jp>
2022-06-26 19:50:40 +0900

    [add] CSS, favicon

--- src/main/java/net/akichil/shusshare/config/SecurityConfig.java
+++ src/main/java/net/akichil/shusshare/config/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
 
         // 認可
         http.authorizeRequests()
-                .antMatchers("/img/**").permitAll()
+                .antMatchers("/img/**", "/css/**").permitAll()
                 .antMatchers("/login", "/logout", "/register/**").permitAll()
                 .antMatchers("/home", "/mypage").hasAuthority("USER")
                 .anyRequest().authenticated();
